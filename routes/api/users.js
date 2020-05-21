476206bdc6eff626f5e056861be3b4b5d9acec74
Jean Ha <jeanha124@gmail.com>
2020-05-21 11:18:45 -0400

    'change login route with jwt and errors'

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,13 +6,20 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   User.findOne({ email })
     .then(user => {
       if (!user) {
-        return res.status(404).json({ email: 'This user does not exist' });
+        errors.email = 'This user does not exist';
+        return res.status(404).json(errors);
       }
 
       bcrypt.compare(password, user.password)
@@ -29,7 +36,8 @@ router.post('/login', (req, res) => {
               }
             );
           } else {
-            return res.status(400).json({ password: 'Incorrect password' });
+            errors.password = 'Incorrect Password';
+            return res.status(400).json(errors);
           }
         })
     })
