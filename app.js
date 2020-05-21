2cb998fa2b52a0581deefc10d8a3c14afc8af356
Jean Ha <jeanha124@gmail.com>
2020-05-21 10:47:16 -0400

    "setup mongoose"

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const db = require('./config/leys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
