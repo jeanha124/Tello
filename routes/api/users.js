7df304b488270ae0670dc99bc98cc9987f407e98
Jean Ha <jeanha124@gmail.com>
2020-05-21 11:02:54 -0400

    "setup user model and registration with bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: 'A user has already registered with this email'});
+      } else {
+        const newUser = new User({
+          email: req.body.email,
+          fullname: req.body.fullname,
+          password: req.body.password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
 
