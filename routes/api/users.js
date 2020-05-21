3cb73e48b3c2abf13265cb747176d0b1eb2a34bd
Jean Ha <jeanha124@gmail.com>
2020-05-21 11:16:20 -0400

    "change register route with jwt"

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,51 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: 'This user does not exist' });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = { id: user.id, email: user.email };
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              {expiresIn: 3600},
+              (err, token) => {
+                res.json({ success: true, token: 'Bearer ' + token });
+              }
+            );
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        })
+    })
+})
 
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
     .then(user => {
       if (user) {
-        return res.status(400).json({ email: 'A user has already registered with this email'});
+        errors.email = "User already exists";
+        return res.status(400).json(errors);
       } else {
         const newUser = new User({
           email: req.body.email,
@@ -20,7 +59,18 @@ router.post('/register', (req, res) => {
             if (err) throw err;
             newUser.password = hash;
             newUser.save()
-              .then(user => res.json(user))
+              .then(user => {
+                const payload = { id: user.id, email: user.email };
+
+                jwt.sign(
+                  payload,
+                  keys.secretOrKey,
+                  {expiresIn: 3600},
+                  (req, res) => {
+                    res.json({ success: true, token: 'Bearer ' + token });
+                  }
+                  );
+              })
               .catch(err => console.log(err));
           })
         })
